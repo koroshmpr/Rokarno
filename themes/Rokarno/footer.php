787f24bed6a3f2aa335db475b2b1e9ee7472247c
korosh.mpr <korosh.mpr@gmail.com>
2024-02-19 13:26:13 +0330

    ver 1.0.8 responsive checked

--- themes/Rokarno/footer.php
+++ themes/Rokarno/footer.php
@@ -9,7 +9,7 @@ if (!is_front_page()) {
 if (is_singular('product')) { ?>
     <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" style="backdrop-filter: blur(8px)">
         <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
-            <div class="modal-content bg-white bg-opacity-10 p-3 border-white border-opacity-25 rounded-0">
+            <div class="modal-content bg-white bg-opacity-10 p-lg-3 border-white border-opacity-25 rounded-0">
                 <div class="modal-body text-center overflow-hidden">
                     <img class="img-fluid position-relative product-image__modal" src="<?= get_field('brand_logo_img', 'option')['url']; ?>" alt="Full-size Image">
                 </div>
